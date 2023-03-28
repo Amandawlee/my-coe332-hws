c2af050b94e683f9f225c5a6ca4983c604fd15ff
Ubuntu <ubuntu@awl-vm.js2local>
2023-03-28 05:33:18 +0000

    final changes to gene_api

--- homework06/gene_api.py
+++ homework06/gene_api.py
@@ -28,20 +28,21 @@ def data() -> list:
     if request.method == 'POST':
         response = requests.get(url = 'https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/json/hgnc_complete_set.json')
         gene_data = response.json()
-        for item in gene_data:
-            key = f"{item['response']['docs']['hgnc_id']}"
-            rd.hset(key, mapping = item)
+
+        for item in gene_data['response']['docs']:
+            key = f"{item['hgnc_id']}"
+            rd.set(key, json.dumps(item))
         return("HGNC data has been loaded to a Redis database.\n")
     elif request.method == 'GET':
         gene_data = []
         for item in rd.keys():
-            gene_data.append(rd.hgetall(item))
+            gene_data.append(json.loads(rd.get(item)))
         return(gene_data)
     elif request.method == 'DELETE':
         rd.flushdb()
-        return("HGNC data has ben deleted from Redis database.\n")
+        return("HGNC data has ben deleted from Redis database. There are {rd.keys()} keys in the database.\n")
     else:
-        return("The method you used is not available.\n")
+        return("The method you tried does not work..\n")
 
 @app.route('/genes', methods = ['GET'])
 def get_hgnc_ids() -> list:
@@ -57,22 +58,21 @@ def get_hgnc_ids() -> list:
     
     hgnc_ids_list = []
     for key in rd.keys():
-        gene_data  = rd.hgetall(key)
-        if gene_data[hgnc_id] not in hgnc_ids_list:
-            hgnc_ids_list.append(gene_data['hgnc_id'])
+        hgnc_ids_list.append(key)
     return(hgnc_ids_list)
 
 @app.route('/genes/<hgnc_id>', methods = ['GET'])
 def get_hgnc_id_data(hgnc_id) -> list:
     """
     """
-    hgnc_id_data = []
-    if hgnc_id in get_hgnc_ids():
-        for key in rd.keys():
-            gene_data = rd.hgetall(key)
-            if gene_data['hgnc_id'] == hgnc_id:
-                hgnc_id_data.append(gene_data)
-    return(hgnc_id_data)
+    if len(rd.keys()) == 0:
+        return("The database is currently empty.\n")
+
+    for key in rd.keys():
+        if str(key) == str(hgnc_id):
+            return(json.loads(rd.get(key))
+        
+    return("HGNC ID doesn't match any IDs in the database.\n")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
